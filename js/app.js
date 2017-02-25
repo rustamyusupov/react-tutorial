5901285468f7dc5dde972b8cad3d0c9848752684
Rustam Yusupov <rustam.yusupov@gmail.com>
2017-02-25 19:56:02 +0500

    using proprs

--- js/app.js
+++ js/app.js
@@ -1,8 +1,36 @@
+var news = [
+  {
+    author: 'Джеймс Кезиах Делейни',
+    text: 'Строит корабельную империю...'
+  },
+  {
+    author: 'Леонард и Шелдон',
+    text: 'Считают себя «великими умами»'
+  },
+  {
+    author: 'Капитан Флинт',
+    text: 'Самый грозный пират в Карибском море'
+  }
+];
+
 const News = React.createClass({
   render: function() {
+    const data = this.props.data;
+
+    const newsTemplate = data.map(function (item, i) {
+      return (
+        <div key={i}>
+          <p className="news__author">{item.author}:</p>
+          <p className="news__text">{item.text}</p>
+        </div>
+      )
+    });
+
+    console.log(newsTemplate);
+
     return (
       <div className='news'>
-        No news today, sorry!
+        {newsTemplate}
       </div>
     );
   }
@@ -12,7 +40,7 @@ const Comments = React.createClass({
   render: function() {
     return (
       <div className='comments'>
-        No comments!
+        Без комментариев!
       </div>
     );
   }
@@ -22,8 +50,8 @@ const App = React.createClass({
   render: function() {
     return (
       <div className='app'>
-        Hi, I a component App!
-        <News />
+        Привет, я реакт компонент App!
+        <News data={news}/>
         <Comments />
       </div>
     );
