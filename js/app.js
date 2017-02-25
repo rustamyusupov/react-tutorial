8c2083fb92f627a981ffafc0660e91c85b011a3b
Rustam Yusupov <rustam.yusupov@gmail.com>
2017-02-25 20:04:50 +0500

    using if-else and ?

--- js/app.js
+++ js/app.js
@@ -15,22 +15,28 @@ var news = [
 
 const News = React.createClass({
   render: function() {
+    let newsTemplate;
     const data = this.props.data;
 
-    const newsTemplate = data.map(function (item, i) {
-      return (
-        <div key={i}>
-          <p className="news__author">{item.author}:</p>
-          <p className="news__text">{item.text}</p>
-        </div>
-      )
-    });
+    if (data.length > 0) {
+      newsTemplate = data.map(function (item, i) {
+        return (
+          <div key={i}>
+            <p className="news__author">{item.author}:</p>
+            <p className="news__text">{item.text}</p>
+          </div>
+        )
+      });
+    } else {
+      newsTemplate = <p>К сожалению новостей нет</p>;
+    }
 
     console.log(newsTemplate);
 
     return (
       <div className='news'>
         {newsTemplate}
+        <strong className={data.length > 0 ? '' : 'none'}>Всего новостей: {data.length}</strong>
       </div>
     );
   }
