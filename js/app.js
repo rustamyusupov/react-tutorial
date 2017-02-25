d2ca93f7acfbdf9957c58213a2dbaa5756616fe1
Rustam Yusupov <rustam.yusupov@gmail.com>
2017-02-25 15:00:36 +0500

    create components: app, news, comments

--- js/app.js
+++ js/app.js
@@ -1,4 +1,37 @@
+const News = React.createClass({
+  render: function() {
+    return (
+      <div className='news'>
+        No news today, sorry!
+      </div>
+    );
+  }
+});
+
+const Comments = React.createClass({
+  render: function() {
+    return (
+      <div className='comments'>
+        No comments!
+      </div>
+    );
+  }
+});
+
+const App = React.createClass({
+  render: function() {
+    return (
+      <div className='app'>
+        Hi, I a component App!
+        <News />
+        <Comments />
+      </div>
+    );
+  }
+});
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <App />,
   document.getElementById('root')
 );
+
